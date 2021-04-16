1c4f5a7eddac1c19c0f48eac0fd3166f49e3794b
olofHammar <70969820+olofHammar@users.noreply.github.com>
2021-04-16 12:49:58 +0200

    added basic navigation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
+import React from 'react';
+import Nav from './components/Nav';
+import Welcome from './components/Home';
+import Dogs from './components/Dogs';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Home from './components/Home';
 
 function App() {
   return (
-    <div>
-      Hello World
+    <Router>
+    <div className="App">
+      <Nav />
+      <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/home" component={Home}/>
+      <Route path="/dogs" component={Dogs}/>
+      </Switch>
     </div>
+    </Router>
   );
 }
 

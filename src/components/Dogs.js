a3f6a4373387f2755cbd4c1db5e1b57644bf50d1
olofHammar <70969820+olofHammar@users.noreply.github.com>
2021-04-19 15:44:09 +0200

    added seperate file for api

--- src/components/Dogs.js
+++ src/components/Dogs.js
@@ -1,36 +1,9 @@
-import React, {useState, useEffect} from 'react';
-import {Link} from 'react-router-dom';
+import Row from './Row';
 
-
-function Dogs(props) {
-
-    const [dogs, setDogs] = useState([]);
-
-    useEffect(() => {
-        fetchDogs();
-    }, []);
-
-    const fetchDogs = async () => {
-        const data = await fetch(
-            'https://api.jsonbin.io/b/5f4d604b514ec5112d136cd6'
-        );
-
-        const dogs = await data.json();
-        //console.table(dogs);
-        setDogs(dogs);
-    }
-
-
+function Dogs() {
     return (
-        <div>
-            {dogs.map(dog => (
-                <Link to={`/dogs/${dog.chipNumber}`} key={dog.chipNumber}>
-                <div key={dog.chipNumber}>
-                <img src={dog.img} alt={dog.name}></img>
-                <h1>{dog.name}</h1>
-                </div>
-                </Link>
-            ))}
+        <div className="dogs">
+            <Row title="Dog Index" />
         </div>
     )
 }

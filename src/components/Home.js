934ba040a19377896d6505ba2309edcb1e8731d9
olofHammar <70969820+olofHammar@users.noreply.github.com>
2021-04-28 16:14:40 +0200

    everything works and looks good in both web and mobile

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ function Home() {
     return (
         <div className="home">
         <div className="home_info">
-            <h2 className="home_welcome">Welcome to</h2>
+           <h2 className="home_welcome">Welcome to</h2>
            <h1 className="home_title">Doggy Daycare</h1>
            <h3>A place for your dog to meet and play with friends.</h3>
            <br></br>
